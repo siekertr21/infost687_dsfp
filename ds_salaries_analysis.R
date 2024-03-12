c02683e4cea659a2e9789f8bd235f19cb05511ef
Ryan Siekert <Siekertr21@gmail.com>
2024-03-11 19:12:35 -0500

    Building initial files

---
+++ ds_salaries_analysis.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(ggplot2)
+library(moments)
+library(tidyverse)
+
+df <- read.csv(file="C:/Users/Sieke/OneDrive/Desktop/Github Repositories/infost687_dsfp/ds_salaries.csv", header=TRUE)
+
+head(df)
+str(df)
