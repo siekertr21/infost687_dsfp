94208d9c5ff6b5e9b139e2862b4989cb2db5d56c
Ryan Siekert <Siekertr21@gmail.com>
2024-04-28 21:54:02 -0500

    Began an initial exploratory analysis

--- ds_salaries_analysis.R
+++ ds_salaries_analysis.R
@@ -1,9 +1,28 @@
-library(dplyr)
-library(ggplot2)
-library(moments)
-library(tidyverse)
-
+#load libraries
+library(dplyr) # for data manipulation
+library(ggplot2) # for data visualization
+library(moments) # for statistical analysis
+library(tidyverse) # for data transformations
+gitcreds::gitcreds_set() # sets github credentials
 df <- read.csv(file="C:/Users/Sieke/OneDrive/Desktop/Github Repositories/infost687_dsfp/ds_salaries.csv", header=TRUE)
 
-head(df)
+#view structure of dataset
 str(df)
+summary(df)
+
+#-----------------------------exploratory analysis-----------------------------
+
+#Histogram showing distribution of salaries by company size
+ggplot(df, aes(x = salary_in_usd))+ 
+  geom_histogram(binwidth = 50000, fill = "skyblue", color = "black")+ #histogram
+  facet_wrap(~company_size, scales="free")+ #breaks into multiple graphs
+  labs(title = "Histogram of Salary", x = "Salaries", y = "Density")+ #labels
+  theme(axis.text.x = element_text(angle=90))+ #angles text
+  scale_x_continuous(breaks=seq(0, 800000, by=50000))+ #sets axis
+  scale_y_continuous(breaks=seq(0, 200, by=20))+ #sets axis
+  theme_minimal()+
+  theme(axis.text.x=element_text(angle=45, hjust=1.5),
+        panel.grid.major.x = element_blank(), 
+        panel.grid.minor.x = element_blank())  # Remove x axis gridlines
+
+
